3664d00d66110201b3d12436336007d527910b9a
lishanyi-66 <you@example.com>
2021-03-01 18:01:41 +0800

    qqq

---
+++ src/components/NavBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-list">
+      
+        <nav-bar-item class="active">
+          <van-icon name="wap-home-o"  size="22" slot="nav-icon"/>
+          <div slot="nav-name">首页</div>
+        </nav-bar-item>
+        <nav-bar-item>
+          <van-icon name="wap-home-o"  size="22" slot="nav-icon"/>
+          <div slot="nav-name">分类</div>
+        </nav-bar-item>
+        <nav-bar-item>
+          <div slot="nav-icon">
+            <van-icon name="wap-home-o"  size="22"/>
+          </div>
+          <div slot="nav-name">购物车</div>
+        </nav-bar-item>
+        <nav-bar-item>
+          <div slot="nav-icon">
+            <van-icon name="wap-home-o"  size="22"/>
+          </div>
+          <div slot="nav-name">我的</div>
+        </nav-bar-item>
+      
+    </div>
+      
+  </div>
+</template>
+
+<script>
+import NavBarItem from './NavBarItem.vue'
+export default {
+  components: { NavBarItem },
+
+}
+</script>
+
+<style lang='less'>
+.nav-bar{
+  width: 100%;
+  height: 2.75rem;
+  background-color: white;
+  box-sizing: border-box;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  border-top: 1px solid #eee;
+  
+}
+.nav-list{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  // color: white;
+  font-size: 10px;
+}
+
+</style>
---
+++ src/components/NavBarItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav-list-item">
+      <slot name='nav-icon'></slot>
+      <slot name='nav-name'></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+  data() {
+    return {
+
+    };
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang='less' scoped>
+.nav-list-item{
+    text-align: center;
+    margin-top: 2px;
+}
+
+</style>
